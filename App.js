fccfdaa9d7a5c6dde8a709bbf18db8957280b0b8
Bayel55555 <92914907+Bayel55555@users.noreply.github.com>
2022-12-12 10:29:02 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Fliter from './companents/Fliter';
+import Banner from './companents/Banner';
+import Cresly from './companents/Cresly';
+import Creslu from './companents/Creslu';
+import Header from './companents/Header';
+import Impuls from './companents/Ampuls';
+import Navbar from './companents/Navbar';
+
+
+
+export default function App() {
+  return (
+    <div>
+      < Navbar />
+      < Header />
+      < Banner />
+      < Cresly />
+      < Impuls />
+      < Creslu />
+      < Fliter />
+    </div>
+  );
+}
+
